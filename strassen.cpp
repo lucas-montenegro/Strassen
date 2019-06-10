5195bd8bc9a9ebaaec87b83d0d013db424c39215
Lucas Montenegro Andrade Assunção <43447533+lucas-montenegro@users.noreply.github.com>
2019-06-10 17:32:45 -0300

    Update strassen.cpp
    Strassen modificado com erros

--- strassen.cpp
+++ strassen.cpp
@@ -22,50 +22,80 @@ void print_matrix(int z) {
 	}
 }
 
-int M7(int i_inicial, int j_inicial) {
-	return (A[i_inicial][j_inicial + 1] - A[i_inicial + 1][j_inicial + 1]) * (B[i_inicial + 1][j_inicial] + B[i_inicial + 1][j_inicial + 1]);
+void create_all_matrices(vector<vector<int>> &s1, vector<vector<int>> &s2, vector<vector<int>> &s3, vector<vector<int>> &s4, vector<vector<int>> &s5, vector<vector<int>> &s6, vector<vector<int>> &s7, vector<vector<int>> &s8, vector<vector<int>> &s9, vector<vector<int>> &s10, int z) {
+	s1.resize(z);
+	s2.resize(z);
+	s3.resize(z);
+	s4.resize(z);
+	s5.resize(z);
+	s6.resize(z);
+	s7.resize(z);
+	s8.resize(z);
+	s9.resize(z);
+	s10.resize(z);
+
+	for(int i = 0; i < z; i++) {
+		s1[i].assign(z, 0);
+		s2[i].assign(z, 0);
+		s3[i].assign(z, 0);
+		s4[i].assign(z, 0);
+		s5[i].assign(z, 0);
+		s6[i].assign(z, 0);
+		s7[i].assign(z, 0);
+		s8[i].assign(z, 0);
+		s9[i].assign(z, 0);
+		s10[i].assign(z, 0);
+	}
 }
 
-int M6(int i_inicial, int j_inicial) {
-	return (A[i_inicial + 1][j_inicial] - A[i_inicial][j_inicial]) * (B[i_inicial][j_inicial] + B[i_inicial][j_inicial + 1]);
+void operation(vector<vector<int>> &matrix_r, vector<vector<int>> &matrix_1, vector<vector<int>> &matrix_2, int i1_inicial, int j1_inicial, int i2_inicial, int j2_inicial, int option, int z) {
+	if(option == 0) { // faz a soma da matriz 1 com a matriz 2
+		for(int i = 0; i < z; i++) {
+			for(int j = 0; j < z; j++) {
+				matrix_r[i][j] = matrix_1[i1_inicial + i][j1_inicial + j] + matrix_2[i2_inicial + i][j2_inicial + j]; 
+			}
+		}
+	}
+	else { // faz a subtração da matriz 1 com a matriz 2
+		for(int i = 0; i < z; i++) {
+			for(int j = 0; j < z; j++) {
+				matrix_r[i][j] = matrix_1[i1_inicial + i][j1_inicial + j] - matrix_2[i2_inicial + i][j2_inicial + j]; 
+			}
+		}
+	}
 }
 
-int M5(int i_inicial, int j_inicial) {
-	return (A[i_inicial][j_inicial] + A[i_inicial][j_inicial + 1]) * B[i_inicial + 1][j_inicial + 1];
-}
-
-int M4(int i_inicial, int j_inicial) {
-	return A[i_inicial + 1][j_inicial + 1] * (B[i_inicial + 1][j_inicial] - B[i_inicial][j_inicial]);
-}
-
-int M3(int i_inicial, int j_inicial) {
-	cout << A[i_inicial][j_inicial] << " " << B[i_inicial][j_inicial + 1] << " " << B[i_inicial + 1][j_inicial + 1] << "\n";
-	return A[i_inicial][j_inicial] * (B[i_inicial][j_inicial + 1] - B[i_inicial + 1][j_inicial + 1]);
-}
-
-int M2(int i_inicial, int j_inicial) {
-	return (A[i_inicial + 1][j_inicial] + A[i_inicial + 1][j_inicial + 1]) * B[i_inicial][j_inicial];
-}
-
-int M1(int i_inicial, int j_inicial) {
-	//cout << A[i_inicial][j_inicial] << " " << A[i_inicial + 1][j_inicial + 1] << " " << B[i_inicial][j_inicial] << " " << B[i_inicial + 1][j_inicial + 1] << "\n";
-	return (A[i_inicial][j_inicial] + A[i_inicial + 1][j_inicial + 1]) * (B[i_inicial][j_inicial] + B[i_inicial + 1][j_inicial + 1]);
-}
-
-void strassen(int i_inicial, int i_final, int j_inicial, int j_final) {
-	if(i_final - i_inicial == 2 && j_final - j_inicial == 2) {
-		cout << M3(i_inicial, j_inicial) << " " << M5(i_inicial, j_inicial) << "\n";
-		C[i_inicial][j_inicial] = M1(i_inicial, j_inicial) + M4(i_inicial, j_inicial) - M5(i_inicial, j_inicial) + M7(i_inicial, j_inicial);
-		C[i_inicial][j_inicial + 1] = M3(i_inicial, j_inicial) + M5(i_inicial, j_inicial);
-		C[i_inicial + 1][j_inicial] = M2(i_inicial, j_inicial) + M4(i_inicial, j_inicial);
-		//C[i_inicial + 1][j_inicial + 1] = M1(i_inicial, j_inicial) - M2(i_inicial, j_inicial) + M3(i_inicial, j_inicial) + M6(i_inicial, j_inicial);
-		return;
+int strassen(vector<vector<int>> &A, vector<vector<int>> &B, int i_inicial, int j_inicial, int z) {
+	if(z == 1) {
+		return A[i_inicial][j_inicial] * B[i_inicial][j_inicial];
 	}
 	else {
-		strassen(i_inicial, i_final / 2, j_inicial, j_final / 2);
-		strassen((i_final / 2), i_final, j_inicial, j_final / 2);
-		strassen(i_inicial, i_final / 2, (j_final / 2), j_final);
-		strassen((i_final / 2), i_final, (j_final / 2), j_final);
+		z /= 2;
+		vector<vector<int>> s1, s2, s3, s4, s5, s6, s7, s8, s9, s10;
+		create_all_matrices(s1, s2, s3, s4, s5, s6, s7, s8, s9, s10, z);
+		operation(s1, B, B, i_inicial, z / 2, z / 2, z / 2, 1, z);
+		operation(s2, A, A, i_inicial, j_inicial, i_inicial, z / 2, 0, z); 
+		operation(s3, A, A, z / 2, j_inicial, z / 2, z / 2, 0, z); 
+		operation(s4, B, B, z / 2, j_inicial, i_inicial, j_inicial, 1, z);
+		operation(s5, A, A, i_inicial, j_inicial, z / 2, z / 2, 0, z);
+		operation(s6, B, B, i_inicial, j_inicial, z / 2, z / 2, 0, z);
+		operation(s7, A, A, i_inicial, z / 2, z / 2, z / 2, 1, z);
+		operation(s8, B, B, z / 2, j_inicial, z / 2, z / 2, 0, z);
+		operation(s9, A, A, i_inicial, j_inicial, z / 2, j_inicial, 1, z);
+		operation(s10, B, B, i_inicial, j_inicial, i_inicial, z / 2, 0, z);
+
+		int p1 = strassen(A, s1, i_inicial, j_inicial, z);
+		int p2 = strassen(s2, B, z / 2, z / 2, z);
+		int p3 = strassen(s3, B, i_inicial, j_inicial, z);
+		int p4 = strassen(A, s4, z / 2, z / 2, z);
+		int p5 = strassen(s5, s6, i_inicial, j_inicial, z);
+		int p6 = strassen(s7, s8, i_inicial, j_inicial, z);
+		int p7 = strassen(s9, s10, i_inicial, j_inicial, z);
+
+		C[i_inicial][j_inicial] = p5 + p4 - p2 + p6;
+		C[i_inicial][z / 2] = p1 + p2;
+		C[z / 2][j_inicial] = p3 + p4;
+		C[z / 2][z / 2] = p5 + p1 - p3 - p7;
 	}
 }
 
@@ -119,14 +149,14 @@ int main() {
 			cin >> B[i][j];
 		}
 	}
-	strassen(0, z, 0, z);
+	strassen(A, B, 0, 0, z);
 
-	/*for(int i = 0; i < z; i++) {
+	for(int i = 0; i < z; i++) {
 		for(int j = 0; j < z; j++) {
 			cout << C[i][j] << " ";
 		}
 		cout << "\n";
-	}*/
+	}
 
 	return 0;
 }
